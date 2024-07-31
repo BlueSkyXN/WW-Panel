98352b010a1c15f4d6ad5e789cae51d658726286
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-07-31 12:32:34 +0800

    0.1.11

--- src/components/GameCalculator.vue
+++ src/components/GameCalculator.vue
@@ -69,22 +69,23 @@
           v-model.number="field.value"
         ></v-text-field>
       </v-col>
+    </v-row>
     
     <v-divider></v-divider>
 
     <v-col cols="12" md="6">
-        <v-divider vertical></v-divider>
-      </v-col>
+      <v-divider vertical></v-divider>
+    </v-col>
 
     <v-col cols="12" md="6">
-        <v-card>
-          <v-card-title>计算结果</v-card-title>
-          <v-card-text>
-            <p>最终实际攻击: {{ finalActualAttack.toFixed(3) }}</p>
-            <p>最终实际伤害基值: {{ finalDamageBase.toFixed(3) }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
+      <v-card>
+        <v-card-title>计算结果</v-card-title>
+        <v-card-text>
+          <p>最终实际攻击: {{ finalActualAttack.toFixed(3) }}</p>
+          <p>最终实际伤害基值: {{ finalDamageBase.toFixed(3) }}</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
     </v-row>
 
     <v-divider></v-divider>
@@ -174,116 +175,72 @@ export default {
         { label: '其他暴击率', model: 'extraCritRate', value: 0.25, type: 'number', step: 0.01 },
         { label: '其他暴击伤害', model: 'extraCritDamage', value: 0.00, type: 'number', step: 0.01 },
         { label: '其他攻击百分比', model: 'extraAttackPercent', value: 0.35, type: 'number', step: 0.01 },
-        { label: '其他攻击固定值', model: 'extraAttackFixed', value: 0.00, type: 'number', step: 1 },
-        { label: '其他元素伤害百分比', model: 'extraElementDamagePercent', value: 0.10, type: 'number', step: 0.01 }
+        { label: '其他攻击固定值', model: 'extraAttackFixed', value: 0.00, type: 'number', step: 0.01 },
+        { label: '其他元素伤害百分比', model: 'extraElementDamagePercent', value: 0.20, type: 'number', step: 0.01 }
       ]
     };
   },
   computed: {
-    finalCritRate() {
-      return (
-        this.basicInfo[0].value + 
-        this.talents[0].value + 
-        this.weapons[0].value + 
-        this.relics[0].value + 
-        this.others[0].value
-      );
-    },
-    finalCritDamage() {
-      return (
-        this.basicInfo[1].value + 
-        this.talents[1].value + 
-        this.weapons[1].value + 
-        this.relics[1].value + 
-        this.others[1].value
-      );
-    },
-    finalAttackPercent() {
-      return (
-        this.talents[2].value + 
-        this.weapons[2].value + 
-        this.relics[2].value + 
-        this.others[2].value
-      );
-    },
-    finalAttackFixed() {
-      return this.relics[3].value + this.others[3].value;
-    },
-    finalElementDamagePercent() {
-      return (
-        this.talents[3].value + 
-        this.weapons[3].value + 
-        this.relics[4].value + 
-        this.others[4].value
-      );
-    },
     finalActualAttack() {
-      const baseAttack = this.basicInfo[2].value + this.basicInfo[3].value;
-      return baseAttack * (1 + this.finalAttackPercent) + this.finalAttackFixed;
+      const baseAttack = this.baseAttack + this.weaponAttack;
+      const attackPercent = this.talentAttackPercent + this.weaponAttackPercent + this.relicAttackPercent + this.extraAttackPercent;
+      return baseAttack * (1 + attackPercent);
     },
     finalDamageBase() {
-      const actualAttack = this.finalActualAttack;
-      return (
-        (actualAttack * this.finalCritRate * this.finalCritDamage + actualAttack * (1 - this.finalCritRate)) * (1 + this.finalElementDamagePercent)
-      );
-    }
-  },
-  methods: {
+      const critRate = this.baseCritRate + this.talentCritRate + this.weaponCritRate + this.relicCritRate + this.extraCritRate;
+      const critDamage = this.baseCritDamage + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage;
+      const elementDamagePercent = this.talentElementDamagePercent + this.weaponElementDamagePercent + this.relicElementDamagePercent + this.extraElementDamagePercent;
+      const baseAttack = this.baseAttack + this.weaponAttack;
+      const attackPercent = this.talentAttackPercent + this.weaponAttackPercent + this.relicAttackPercent + this.extraAttackPercent;
+      const finalAttack = baseAttack * (1 + attackPercent);
+      return finalAttack * (1 + critRate * critDamage + elementDamagePercent);
+    },
     percentCritRateIncrease() {
-      const originalDamageBase = this.finalDamageBase;
-      this.basicInfo[0].value += 0.01;
-      const newDamageBase = this.finalDamageBase;
-      this.basicInfo[0].value -= 0.01;
-      return ((newDamageBase - originalDamageBase) / originalDamageBase) * 100;
+      const oldBase = this.finalDamageBase;
+      const newCritRate = this.baseCritRate + 0.01 + this.talentCritRate + this.weaponCritRate + this.relicCritRate + this.extraCritRate;
+      const newBase = this.finalDamageBase * (1 + newCritRate * (this.baseCritDamage + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage));
+      return ((newBase - oldBase) / oldBase) * 100;
     },
     percentCritDamageIncrease() {
-      const originalDamageBase = this.finalDamageBase;
-      this.talents[1].value += 0.01;
-      const newDamageBase = this.finalDamageBase;
-      this.talents[1].value -= 0.01;
-      return ((newDamageBase - originalDamageBase) / originalDamageBase) * 100;
+      const oldBase = this.finalDamageBase;
+      const newCritDamage = this.baseCritDamage + 0.01 + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage;
+      const critRate = this.baseCritRate + this.talentCritRate + this.weaponCritRate + this.relicCritRate + this.extraCritRate;
+      const newBase = this.finalDamageBase * (1 + critRate * newCritDamage);
+      return ((newBase - oldBase) / oldBase) * 100;
     },
     percentAttackPercentIncrease() {
-      const originalDamageBase = this.finalDamageBase;
-      this.talents[2].value += 0.01;
-      const newDamageBase = this.finalDamageBase;
-      this.talents[2].value -= 0.01;
-      return ((newDamageBase - originalDamageBase) / originalDamageBase) * 100;
+      const oldBase = this.finalDamageBase;
+      const newAttackPercent = this.talentAttackPercent + this.weaponAttackPercent + this.relicAttackPercent + this.extraAttackPercent + 0.01;
+      const baseAttack = this.baseAttack + this.weaponAttack;
+      const finalAttack = baseAttack * (1 + newAttackPercent);
+      const newBase = finalAttack * (1 + this.baseCritRate * (this.baseCritDamage + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage) + this.talentElementDamagePercent + this.weaponElementDamagePercent + this.relicElementDamagePercent + this.extraElementDamagePercent);
+      return ((newBase - oldBase) / oldBase) * 100;
     },
     tenPercentAttackPercentIncrease() {
-      const originalDamageBase = this.finalDamageBase;
-      this.talents[2].value += 0.10;
-      const newDamageBase = this.finalDamageBase;
-      this.talents[2].value -= 0.10;
-      return ((newDamageBase - originalDamageBase) / originalDamageBase) * 100;
+      const oldBase = this.finalDamageBase;
+      const newAttackPercent = this.talentAttackPercent + this.weaponAttackPercent + this.relicAttackPercent + this.extraAttackPercent + 0.10;
+      const baseAttack = this.baseAttack + this.weaponAttack;
+      const finalAttack = baseAttack * (1 + newAttackPercent);
+      const newBase = finalAttack * (1 + this.baseCritRate * (this.baseCritDamage + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage) + this.talentElementDamagePercent + this.weaponElementDamagePercent + this.relicElementDamagePercent + this.extraElementDamagePercent);
+      return ((newBase - oldBase) / oldBase) * 100;
     },
     percentElementDamagePercentIncrease() {
-      const originalDamageBase = this.finalDamageBase;
-      this.talents[3].value += 0.01;
-      const newDamageBase = this.finalDamageBase;
-      this.talents[3].value -= 0.01;
-      return ((newDamageBase - originalDamageBase) / originalDamageBase) * 100;
+      const oldBase = this.finalDamageBase;
+      const newElementDamagePercent = this.talentElementDamagePercent + this.weaponElementDamagePercent + this.relicElementDamagePercent + this.extraElementDamagePercent + 0.01;
+      const baseAttack = this.baseAttack + this.weaponAttack;
+      const finalAttack = baseAttack * (1 + this.talentAttackPercent + this.weaponAttackPercent + this.relicAttackPercent + this.extraAttackPercent);
+      const newBase = finalAttack * (1 + this.baseCritRate * (this.baseCritDamage + this.talentCritDamage + this.weaponCritDamage + this.relicCritDamage + this.extraCritDamage) + newElementDamagePercent);
+      return ((newBase - oldBase) / oldBase) * 100;
     }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 .v-container {
   max-width: 1000px;
-  margin: 0 auto;
 }
-
-.v-row {
-  flex-wrap: wrap;
-}
-
-.v-col {
-  padding: 8px;
-}
-
-.v-divider.vertical {
-  height: 100%;
-  margin: 0;
+.v-card {
+  margin-bottom: 16px;
 }
 </style>
